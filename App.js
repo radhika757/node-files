052a4da12c1a36f3476b7ca41b0fec7d52b38b7e
Radhika Rao <raoradhika2000@gmail.com>
2023-03-26 18:43:10 +0530

    First application

---
+++ App.js
@@ -0,0 +1,14 @@
+const amount = 12;
+
+if (amount < 10) {
+  console.log("Smol");
+} else {
+  console.log("Big");
+}
+
+console.log("My first node application");
+console.log(__dirname);
+// console.log(process);
+// setInterval(() => {
+//   console.log("Hello world");
+// }, 1000);
